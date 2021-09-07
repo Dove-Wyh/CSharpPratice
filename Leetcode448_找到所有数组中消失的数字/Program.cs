3cb6281cd0271e5bdb86f0c35492b421edf3a92a
35446 <35446@DESKTOP-FB2IFBC>
2021-09-08 00:26:29 +0800

    添加项目文件。

---
+++ Leetcode448_找到所有数组中消失的数字/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Leetcode448_找到所有数组中消失的数字
+{
+    class Program
+    {
+        public static IList<int> FindDisappearedNumbers(int[] nums)
+        {
+            IList<int> list = new List<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int index = Math.Abs(nums[i]) - 1;
+                if (nums[index] > 0)
+                {
+                    nums[index] = -nums[index];
+                }
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] > 0)
+                {
+                    list.Add(i + 1);
+                }
+            }
+            return list;
+        }
+
+        public static IList<int> FindDisappearedNumbers2(int[] nums)
+        {
+            IList<int> list = new List<int>();
+            HashSet<int> set = new HashSet<int>(nums);
+            for (int i = 1; i <= nums.Length; i++)
+            {
+                if (!set.Contains(i))
+                    list.Add(i);
+            }
+            return list;
+        }
+
+        public static IList<int> FindDisappearedNumberAndAppearedNumber(int[] nums)
+        {
+            IList<int> list = new List<int>();
+            //先寻找不存在的数字
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int index = Math.Abs(nums[i]) - 1;
+                if (nums[index] > 0)
+                {
+                    nums[index] = -nums[index];
+                }
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] > 0)
+                {
+                    list.Add(i + 1);
+                }
+            }
+            //再寻找重复的数字
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int index = Math.Abs(nums[i]) - 1;
+                nums[index] = -nums[index];
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] < 0)
+                {
+                    list.Add(i + 1);
+                }
+            }
+            return list;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            IList<int> result1 = FindDisappearedNumbers(new int[] { 1, 2, 6, 7, 7, 8, 4, 9, 9 });
+            IList<int> result2 = FindDisappearedNumberAndAppearedNumber(new int[] { 1, 2, 6, 7, 7, 8, 4, 3, 5 });
+        }
+    }
+}
---
+++ 剑指Offer022_链表中倒数第K个节点/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace 剑指Offer022_链表中倒数第K个节点
+{
+    class Program
+    {
+        public class ListNode
+        {
+            public int value;
+            public ListNode next;
+            public ListNode(int value, ListNode next = null)
+            {
+                this.value = value;
+                this.next = next;
+            }
+        };
+
+        public static ListNode InitList(int length)
+        {
+            ListNode headNode = new ListNode(0);
+            ListNode node = headNode;
+            for (int i = 1; i < length; i++)
+            {
+                node.next = new ListNode(i);
+                node = node.next;
+            }
+            PrintList(headNode);
+            return headNode;
+        }
+
+        public static void PrintList(ListNode node)
+        {
+            while (node != null)
+            {
+                Console.Write(node.value);
+                Console.Write(" ");
+                node = node.next;
+            }
+            Console.WriteLine();
+        }
+
+        public static ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            if (head == null || n == 0)
+            {
+                return null;
+            }
+            ListNode node1 = head;
+            ListNode node2 = head;
+            for (int i = 0; i < n; i++)
+            {
+                if (node1.next != null)
+                {
+                    node1 = node1.next;
+                }
+                else //走到这里说明倒数第n个节点为首节点,直接返回下一个即可
+                {
+                    return head.next;
+                }
+            }
+            while (node1.next != null)
+            {
+                node1 = node1.next;
+                node2 = node2.next;
+            }
+            node2.next = node2.next.next;
+            return head;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WTF");
+            PrintList(RemoveNthFromEnd(InitList(10), 4));
+            PrintList(RemoveNthFromEnd(InitList(2), 2));
+        }
+    }
+}
