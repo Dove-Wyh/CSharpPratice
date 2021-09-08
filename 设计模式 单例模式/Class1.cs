04d5f30a30f01ca31f7646164d37297534d2c9b0
354468791 <354468791@qq.com>
2021-09-09 00:39:34 +0800

    学习C#静态构造函数,静态变量与静态函数

---
+++ 设计模式 单例模式/Class1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 设计模式_单例模式
+{
+    class Class1
+    {
+        public static int a = 1;
+
+        static Class1()
+        {
+            Console.WriteLine("Class1 的静态构造函数");
+        }
+
+        public Class1()
+        {
+            Console.WriteLine("Class1 的构造函数");
+        }
+    }
+
+    class Class2 : Class1
+    {
+        public static int aa = 2;
+
+        static Class2()
+        {
+            Console.WriteLine("Class2 的静态构造函数");
+        }
+
+        public Class2()
+        {
+            Console.WriteLine("Class2 的构造函数");
+        }
+    }
+
+    class Class3 : Class2
+    {
+        public static int aaa = 3;
+
+        static Class3()
+        {
+            Console.WriteLine("Class3 的静态构造函数");
+        }
+
+        public Class3()
+        {
+            Console.WriteLine("Class3 的构造函数");
+        }
+
+        public static void Print()
+        {
+            Console.WriteLine("Class3 的静态成员函数");
+        }
+    }
+
+    class Class4
+    {
+        public static int a = 4;
+
+        static Class4()
+        {
+            Console.WriteLine("Class4 的静态构造函数");
+            Console.WriteLine(Class5.a);
+        }
+
+        public static void Print()
+        {
+            Console.WriteLine("Class4 的静态成员函数");
+        }
+    }
+
+    class Class5
+    {
+        public static int a = 5;
+
+        static Class5()
+        {
+            Console.WriteLine("Class5 的静态构造函数");
+        }
+    }
+}
--- 设计模式 单例模式/Program.cs
+++ 设计模式 单例模式/Program.cs
@@ -4,13 +4,24 @@ namespace 设计模式_单例模式
 {
     class SingletonPratice
     {
+        //1.静态构造函数没有参数,但是可以与无参构造函数共存
+        //2.静态构造函数既没有访问修饰符,不可以被继承,并且只能有一个,且只会调用一次,是线程安全的
+        //3.静态构造函数只会在在创建第一个类实例或任何静态成员被引用时时调用,当有一个静态变量被引用时就初始化所有静态变量
+        //4.如果没有写静态构造函数,而类中包含带有初始值设定的静态成员,那么编译器会自动生成默认的静态构造函数
+
+        static SingletonPratice()
+        {
+            Console.WriteLine("这个可以不写的1");
+        }
+
+        int a = 1;
         //定义私有构造函数避免外部创建实例
         private SingletonPratice()
         {
-
+            Console.WriteLine("单例类的构造函数!");
         }
 
-        //静态构造函数只会在第一次被使用时调用,且只会调用一次,是线程安全的
+        
         public static SingletonPratice Instance
         {
             get
@@ -21,10 +32,10 @@ namespace 设计模式_单例模式
 
         class Nested
         {
-            //定义私有构造函数避免SingletonPratice再创建实例 如
+            //如果没有写静态构造函数,而类中包含带有初始值设定的静态成员,那么编译器会自动生成默认的静态构造函数
             static Nested()
             {
-                Console.WriteLine("这个不知道是干嘛的");
+                Console.WriteLine("这个可以不写的2");
             }
             internal static SingletonPratice instance = new SingletonPratice();
         }
@@ -41,6 +52,10 @@ namespace 设计模式_单例模式
         {
             Console.WriteLine("Hello World!");
             SingletonPratice.Instance.Print();
+
+            //静态构造函数,静态成员变量,静态成员函数实验
+            //Class1 c = new Class3();
+            //Class4.Print();
         }
     }
 }
